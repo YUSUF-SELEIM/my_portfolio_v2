e2cafb6f64cd4baa14bfb547a8f1af65842effd0
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-03-09 01:33:32 +0200

    Refactor Navbar component to include a like button and count

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,47 @@
 'use client';
 import React, { useState } from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link,Button} from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import { FaHeart } from 'react-icons/fa';
 export default function NavbarComponent() {
-
-
-const [filled, setFilled] = useState(false);
-
-const handleClick = () => {
-    setFilled(!filled);
-};
-
-return (
-    <Navbar className="px-2 py-6">
-        <NavbarBrand>
-            <p className="font-bold text-inherit">Yusuf Abdelfattah</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-8" justify="center">
-            <NavbarItem>
-                <Link color="foreground" href="#">
-                    Portfolio
-                </Link>
-            </NavbarItem>
-            <NavbarItem isActive>
-                <Link href="#" aria-current="page">
-                    About
-                </Link>
-            </NavbarItem>
-            <NavbarItem>
-                <Link color="foreground" href="#">
-                    Contact
-                </Link>
-            </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-            <NavbarItem>
-                <Button isIconOnly color="danger" aria-label="Like">
-                    <FaHeart />
-                </Button>   
-            </NavbarItem>
-        </NavbarContent>
-    </Navbar>
-)
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+    return (
+        <>
+            <Navbar className="hidden md:flex px-2 py-3 sticky">
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">Yusuf Abdelfattah</p>
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-8" justify="center">
+                    <NavbarItem>
+                        <Link color="foreground" href="#projects">
+                            Portfolio
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#about">
+                            About
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#contact">
+                            Contact
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <NavbarItem>
+                        <div className="flex space-x-2 items-center justify-between">
+                            <span className="text-xs text-red-700">I Love This Website </span>
+                            <Button isIconOnly color="danger" aria-label="Like" onClick={handleClick}>
+                                <FaHeart />
+                            </Button>
+                            <span className="text-xs text-red-700 font-extrabold">{count}</span>
+                        </div>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+        </>
+    );
 }
