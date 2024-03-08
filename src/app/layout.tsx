f39be14fe93146f65418afc897d43029c14b24ef
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-03-09 01:34:56 +0200

    Update layout and added the components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className='dark scroll-smooth overflow-x-hidden'>
       <Providers>
         <body>{children}</body>
       </Providers>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { Main } from "../components/Main";
 import NavbarComponent from "../components/Navbar";
+import Projects from "../components/Projects";
+import About from "../components/About";
+import { Meteors } from "../components/ui/meteors";
+import Contact from "../components/Contact";
+import { WavyBackground } from "../components/ui/wavy-background";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full">
+    <div>
       <NavbarComponent></NavbarComponent>
       <Main></Main>
+      <Projects></Projects>
+      <About></About>
+
+      <Contact></Contact>
+      {/* <Meteors number={50} /> */}
+
     </div>
   );
 }
