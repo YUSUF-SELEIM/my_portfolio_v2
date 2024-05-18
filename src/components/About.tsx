f3f3893ec538f536768f310927a5ac0dbdb4c4f7
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-18 18:50:02 +0300

    Update About component UI and responsiveness

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -52,20 +52,20 @@ const testimonials = [
 ];
 export default function About() {
   return (
-    <div className="h-screen md:pt-24 pt-5 flex flex-col space-y-5 bg-black" id="about">
+    <div className="md:h-screen md:pt-24 pt-5 flex flex-col space-y-5 bg-black" id="about">
       <div>
         <TypewriterEffectSmooth
           words={[{ text: "About" }]}
           className="text-3xl pb-0 my-4 px-4"
         ></TypewriterEffectSmooth>
-        <div className="flex justify-center py-6">
+        <div className="flex md:flex-row flex-col justify-center py-6">
           <p className="text-2xl px-4 font-light text-neutral-400">
             Hi, I&apos;m Yusuf, a passionate self-motivated web developer with a strong experience in building websites. <br />
             I&apos;m proficient with React, NextJS, Express.js, and MongoDB. <br />
             I&apos;ve used my creativity and problem-solving skills to design and implement websites for clients. <br /></p>
           <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
             <InfiniteMovingCards
-              className="hidden md:block"
+              className=""
               items={testimonials}
               direction="right"
               speed="slow"
@@ -74,8 +74,10 @@ export default function About() {
           </div>
         </div>
       </div>
-      <WavyBackground className="max-w-xl mx-auto mt-10">
+  <div className="hidden md:block h-[5vh]">
+  <WavyBackground className="max-w-xl mx-auto mt-10">
       </WavyBackground>
+  </div>
     </div>
   )
 }
