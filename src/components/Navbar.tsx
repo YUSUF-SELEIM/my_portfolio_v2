131e5cb230cffd28277524b3e6a6f4faa03810ea
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-05-18 18:48:11 +0300

    Removed love button state and logic from NavbarComponent to be a a server component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,12 @@
-'use client';
-import React, { useState } from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import { FaHeart } from 'react-icons/fa';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+import LoveButton from "./LoveButton";
 export default function NavbarComponent() {
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        setCount(count + 1);
-    };
     return (
         <>
             <Navbar className="hidden md:flex px-1 py-3 sticky text-white">
                 <NavbarBrand>
                     <a
-                        href="https://flowcv.com/resume/vkvq6n0r9o"
+                        href="https://www.overleaf.com/read/ggjzkxhgfgsf#f0cabe"
                         className="font-bold text-inherit"
                         target="_blank"
                     >
@@ -36,17 +30,11 @@ export default function NavbarComponent() {
                         </Link>
                     </NavbarItem>
                 </NavbarContent>
-                {/* <NavbarContent className="w-full flex justify-between" justify="end">
+                <NavbarContent className="w-full flex justify-between" justify="end">
                     <NavbarItem>
-                        <div className="flex space-x-2 items-center justify-between">
-                            <span className="text-xs text-red-700">I Love This Website </span>
-                            <Button isIconOnly color="danger" aria-label="Like" onClick={handleClick}>
-                                <FaHeart />
-                            </Button>
-                            <span className="text-xs text-red-700 font-extrabold">{count}</span>
-                        </div>
+                    <LoveButton />
                     </NavbarItem>
-                </NavbarContent> */}
+                </NavbarContent>
             </Navbar>
         </>
     );
