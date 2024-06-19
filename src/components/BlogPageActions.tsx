a01b8bf98a49c372f668fd46b7314807db09f641
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-06-19 22:41:13 +0300

    restyled the like button

--- src/components/BlogPageActions.tsx
+++ src/components/BlogPageActions.tsx
@@ -1,17 +1,21 @@
 'use client';
 import { likeBlogPost } from '@/lib/actions';
 import { Button } from '@nextui-org/react';
-import React from 'react'
+import React, { useState } from 'react'
 import { FaHeart } from 'react-icons/fa';
 
 function BlogPageActions({ blogId, blogLikes }: { blogId: string, blogLikes: number }) {
+    const [likes, setLikes] = useState(blogLikes);
+
     const handleLike = async () => {
         try {
             await likeBlogPost(blogId);
+            setLikes(likes + 1);
         } catch (error) {
             console.error('Error liking blog post:', error);
         }
     };
+
     return (
         <>
             <div className="flex justify-end items-center w-full space-x-2">
@@ -19,13 +23,14 @@ function BlogPageActions({ blogId, blogLikes }: { blogId: string, blogLikes: num
                     isIconOnly
                     onClick={handleLike}
                     color={'danger'}
+                    className='hover:scale-110'
                 >
-                    <FaHeart />
+                    <FaHeart className='hover:animate-pulse' />
                 </Button>
-                <span className='text-xs text-gray-500'>{blogLikes} {blogLikes === 1 ? 'Like' : 'Likes'}</span>
+                <span className='text-xs text-gray-500'>{likes} {likes === 1 ? 'Like' : 'Likes'}</span>
             </div>
         </>
-    )
+    );
 }
 
-export default BlogPageActions
+export default BlogPageActions;
