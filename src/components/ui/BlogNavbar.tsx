f7cfb9413ff0b68a9d1784e1fc7776cbc8fdbc01
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-06-19 22:49:41 +0300

    Added sign in button

--- src/components/ui/BlogNavbar.tsx
+++ src/components/ui/BlogNavbar.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { Navbar as NextUINavbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, useDisclosure, NavbarMenuToggle, Button, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { Navbar as NextUINavbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenuToggle, Button, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import AvatarDropDown from "./AvatarDropDown";
 import { useState } from "react";
+import { FaUserLock } from "react-icons/fa";
 
 export const BlogNavbar = ({ userData }: { userData: any }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -47,7 +49,11 @@ export const BlogNavbar = ({ userData }: { userData: any }) => {
       </NavbarContent>
       <NavbarContent className="w-full flex justify-between" justify="end">
         <NavbarItem>
-          {userData && <AvatarDropDown userData={userData} />}
+          {userData ? <AvatarDropDown userData={userData} /> :
+            <Button color="primary" variant="shadow" startContent={<FaUserLock />}>
+              <LoginLink>Sign in</LoginLink>
+            </Button>
+          }
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
