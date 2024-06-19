67cfc4285472cce0772f08c14ed02de41e0c9abb
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-06-20 00:31:55 +0300

    wrapped MarkdownEditor in a Suspense

--- src/app/blog/editor/page.tsx
+++ src/app/blog/editor/page.tsx
@@ -1,11 +1,12 @@
 import MarkdownEditor from '@/src/components/MarkdownEditor'; 
 import { BlogNavbar } from '@/src/components/ui/BlogNavbar';
+import { Suspense } from "react"
 
 function BlogEditor() {
   return (
     <div className='h-screen flex flex-col'>
       <BlogNavbar userData={null}></BlogNavbar>
-      <MarkdownEditor></MarkdownEditor>
+      <Suspense><MarkdownEditor></MarkdownEditor></Suspense>
     </div>
   )
 }
